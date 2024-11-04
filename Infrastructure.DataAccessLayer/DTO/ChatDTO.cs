3deac4b42a95396de3f012717e05c7a14698bc6d
Raukie <90243019+Raukie@users.noreply.github.com>
2024-11-04 15:07:51 +0100

    #2 Added database create script

--- Infrastructure.DataAccessLayer/DTO/ChatDTO.cs
+++ Infrastructure.DataAccessLayer/DTO/ChatDTO.cs
@@ -7,11 +7,12 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.DTO
 {
-    public class ChatDTO(string name, List<UserDTO> users, List<MessageDTO> messages, int lastReadMessageID)
+    public class ChatDTO(string name, List<UserDTO> users, List<MessageDTO> messages, int lastReadMessageID, string hash)
     {
         public string Name { get; set; } = name;
         public List<UserDTO> Users { get; set; } = users;
         public List<MessageDTO> Messages { get; set; } = messages;
         public int LastReadMessageID { get; set; } = lastReadMessageID;
+        public string Hash { get; set; } = hash;
     }
 }
